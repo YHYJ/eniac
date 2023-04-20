8c6d2419654aad80e4d9ab100b2b07bf83ce2464
YJ-ArchLinux <yj1516268@outlook.com>
2023-04-20 09:54:05 +0800

    +: `version` subcommand

---
+++ cmd/version.go
@@ -0,0 +1,33 @@
+/*
+File: version.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-04-20 09:52:25
+
+Description: 程序子命令'version'时执行
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/yhyj/eniac/function"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "打印程序版本",
+	Long:  `显示版本信息并退出`,
+	Run: func(cmd *cobra.Command, args []string) {
+		programInfo := function.ProgramInfo()
+		fmt.Printf(programInfo)
+	},
+}
+
+func init() {
+	versionCmd.Flags().BoolP("help", "h", false, "Help for version")
+	rootCmd.AddCommand(versionCmd)
+}
---
+++ function/version.go
@@ -0,0 +1,23 @@
+/*
+File: version.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-04-20 09:50:55
+
+Description: 子命令`version`功能函数
+*/
+
+package function
+
+import "fmt"
+
+// 程序信息
+var (
+	name    string = "Eniac"
+	version string = "v0.0.1"
+)
+
+func ProgramInfo() string {
+	programInfo := fmt.Sprintf("\033[1m%s\033[0m %s \033[1m%s\033[0m\n", name, "version", version)
+	return programInfo
+}
