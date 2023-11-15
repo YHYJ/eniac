6656d2ee20cf11372874a6c506108972def6556e
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-15 10:23:19 +0800

    统一相同部分

--- general/define_filemanager.go
+++ general/define_filemanager.go
@@ -10,12 +10,82 @@ Description: 文件操作
 package general
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 	"sync"
 )
+
+// ReadFileLine 读取文件指定行
+func ReadFileLine(file string, line int) string {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 行计数
+	count := 1
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if line == count {
+			return scanner.Text()
+		}
+		count++
+	}
+	return ""
+}
+
+// ReadFileKey 读取文件包含指定字符串的行
+func ReadFileKey(file, key string) string {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			return scanner.Text()
+		}
+	}
+	return ""
+}
+
+// ReadFileCount 获取文件包含指定字符串的行的计数
+func ReadFileCount(file, key string) int {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 计数器
+	count := 0
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			count++
+		}
+	}
+	return count
+}
 
 // FileExist 判断文件是否存在
 func FileExist(filePath string) bool {
