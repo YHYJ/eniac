fb2f910fd42f033a2596dbdbd9a473699bfcd094
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-06-21 15:37:17 +0800

    修复 AskUser 无法返回默认值的问题

---
+++ general/define_flag.go
@@ -0,0 +1,16 @@
+/*
+File: define_flag.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2024-05-29 15:48:58
+
+Description: 定义符号
+*/
+
+package general
+
+var (
+	Separator1st = "=" // 分隔符 - 1级
+	Separator2st = "-" // 分隔符 - 2级
+	Separator3st = "·" // 分隔符 - 3级
+)
