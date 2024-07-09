0882a743fc070c80b4ea2f83e3b2fd102aba2394
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-07-09 09:47:13 +0800

    文件整理

--- general/define_datetime.go
+++ general/define_datetime.go
@@ -9,7 +9,10 @@ Description: 关于日期和时间的方法
 
 package general
 
-import "path"
+import (
+	"path"
+	"time"
+)
 
 // GetTimeZoneOriginal 获取时区信息的原始方法（检测 /etc/localtime 实际指向的文件）
 //
@@ -37,4 +40,33 @@ func GetTimeZoneOriginal() string {
 	}
 
 	return timeZone
+}
+
+// UnixTime2DayHourMinuteSecond Unix 时间戳转换为天、小时、分钟、秒
+//
+// 参数：
+//   - totalSeconds: Unix 时间戳
+//
+// 返回：
+//   - day: 天
+//   - hour: 小时
+//   - minute: 分钟
+//   - second: 秒
+func UnixTime2DayHourMinuteSecond(unixTime int64) (day, hour, minute, second int64) {
+	day = unixTime / 86400
+	hour = (unixTime - day*86400) / 3600
+	minute = (unixTime - day*86400 - hour*3600) / 60
+	second = unixTime - day*86400 - hour*3600 - minute*60
+	return day, hour, minute, second
+}
+
+// UnixTime2TimeString Unix 时间戳转换为字符串格式
+//
+// 参数：
+//   - timeStamp: Unix 时间戳
+//
+// 返回：
+//   - 格式化的 Unix 时间戳字符串
+func UnixTime2TimeString(unixTime int64) string {
+	return time.Unix(unixTime, 0).Format("2006-01-02 15:04:05")
 }
