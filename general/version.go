836f78f22d175e6fa22ba4f15f81b7a97259a6cc
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-07-22 10:03:17 +0800

    v1.8.1

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Eniac"                 // 程序名
-	Version string = "v1.8.0"                // 程序版本
+	Version string = "v1.8.1"                // 程序版本
 	Project string = "github.com/yhyj/eniac" // 项目地址
 )
 
